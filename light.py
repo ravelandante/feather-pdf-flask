cdd4f7b0ea79792ac4e5e3821d5896996897b5b0
MORPHEUS\Fynn <fynn@fynnyoung.com>
2023-01-23 15:49:48 +0200

    add working functionality of simple merge, compress, rotation fix

--- light.py
+++ light.py
@@ -8,7 +8,9 @@ cmd = input().split(' ')
 if cmd[0] == 'merge':
     p.simple_merge(cmd[1].split(','), cmd[2])
 elif cmd[0] == 'compress':
-    p.compress(cmd[1].split(','), cmd[2])
+    p.compress(cmd[1].split(','), cmd[2].split(','))
 elif cmd[0] == 'fix_rotation':
     type = 0 if cmd[3] == 'p' else 90
-    p.fix_rotation(cmd[1].split(','), cmd[2].split(','), type)
+    out_names = cmd[2].split(',') if len(cmd[2]) > 1 else [cmd[2]]
+
+    p.fix_rotation(cmd[1].split(','), out_names, type)
